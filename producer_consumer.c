763e1493fec04771d7a1262440558f928d55e74c
James ONeal <onealja@os-class.engr.oregonstate.edu>
2016-10-08 14:34:55 -0700

    pthreads init

--- producer_consumer.c
+++ producer_consumer.c
@@ -3,5 +3,34 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <errno.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+
+#include "mt19937ar.c"
+
+struct item {
+   	int val;
+	int work;
+};
+
+struct item buffer[32];
+
+pthread_mutex_t mutex;
+pthread_cond_t condConsume = PTHREAD_COND_INITIALIZER;
+pthread_cond_t condProduce = PTHREAD_COND_INITIALIZER;
+pthread_t produce;
+pthread_t consume;
+
+void interruptHandler(int signal)
+{
+   	if(signal == SIGINT)
+	   	{
+		   	pthread_detach(produce);
+			pthread_detach(consume);
+			exit(EXIT_SUCCESS);
+		}
+}
 
 
