116d9453a161ef10e99c8512d3ef473ca63a42ed
James ONeal <onealja@os-class.engr.oregonstate.edu>
2016-10-08 15:12:54 -0700

    producer funtion

--- producer_consumer.c
+++ producer_consumer.c
@@ -16,6 +16,7 @@ struct item {
 };
 
 struct item buffer[32];
+int index = 0;
 
 pthread_mutex_t mutex;
 pthread_cond_t condConsume = PTHREAD_COND_INITIALIZER;
@@ -32,5 +33,28 @@ void interruptHandler(int signal)
 			exit(EXIT_SUCCESS);
 		}
 }
+
+void *producer()
+{
+   	//place to put item created
+	struct item create;
+	int i = 0;
+
+	//random sleep
+	int rnd = genrand_int32() % 5 + 3;
+	while(1){
+	   	sleep(rnd);
+		printf("next production in %d", rnd);
+		//lock buffer
+		pthread_mutex_lock(&mutex);
+		//check buffer
+		while(index == 32){
+		   	pthread_cond_wait(&condProduce, &mutex);
+			printf("space filled, waiting for consumtion...");
+		}
+		//values of item to be created
+		create.val = genrand_int32() % 10;
+		create.work = genrand_int32() %
+
 
 
